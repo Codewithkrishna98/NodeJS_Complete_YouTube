0fecc93a2450f0007796c3b4ec9028a4f01d4597
Codewithkrishna98 <143336373+Codewithkrishna98@users.noreply.github.com>
2025-07-16 21:54:10 +0530

    Update todoItemsController.js
    we can also use create method  instead of save to save task in database

--- Chapter 21 - REST API and Json Requests /todo-backend/controllers/todoItemsController.js
+++ Chapter 21 - REST API and Json Requests /todo-backend/controllers/todoItemsController.js
@@ -2,9 +2,7 @@ const TodoItem = require("../models/TodoItem");
 
 exports.createTodoItem = async (req, res, next) => {
   console.log(req.body);
-  const { task, date } = req.body;
-  const todoItem = new TodoItem({ task, date });
-  await todoItem.save();
+  const todoItem =  await ToddItem.create(task.req.body , date.req.body)         
   res.status(201).json(todoItem);
 }
 
